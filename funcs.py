c5871ae58a1cd87d0029906942ea1487f6c0a353
Debil Yebaniy <bevstrogan@gmail.com>
2023-10-30 09:18:26 +0300

    sucsess import Plus

--- funcs.py
+++ funcs.py
@@ -1,4 +1,4 @@
 def Plus(number1, number2):
-    return number1 + number2
+    return (number1 + number2)
 def Minus(number1, number2):
-    return number1 - number2
+    return (number1 - number2)
---
+++ main.py
@@ -0,0 +1,2 @@
+from funcs import Plus
+print(Plus(1, 2))
